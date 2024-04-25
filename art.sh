5e44c991acea11aafd51479e99d2e91684de6ca8
pinkskies0919 <160773499+pinkskies0919@users.noreply.github.com>
2024-04-25 20:19:44 +0800

    Update art.sh

--- art.sh
+++ art.sh
@@ -1,8 +1,8 @@
 # 使用expect命令输入密码
 expect -c '
-spawn artelad tx staking delegate \$validator $amountart --from \$address --chain-id artela_11822-1 --gas 300000 --node tcp://47.254.66.177:26657
+spawn artelad tx staking delegate $validator $amountart --from $address --chain-id artela_11822-1 --gas 300000 --node tcp://47.254.66.177:26657
 expect "Enter keyring passphrase (attempt 1/3):"
-send "\$pwd\\n"
+send "$pwd\\n"
 expect {
     -re {confirm transaction before signing and broadcasting \[y/N\]:\s*$} {
         send "y\n"
