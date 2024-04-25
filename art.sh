caa3ad32c4d6be47729e5a7590771b3586c7a968
pinkskies0919 <160773499+pinkskies0919@users.noreply.github.com>
2024-04-25 20:31:45 +0800

    Update art.sh

--- art.sh
+++ art.sh
@@ -2,10 +2,10 @@
 expect -c '
 spawn artelad tx staking delegate $validator $amountart --from $address --chain-id artela_11822-1 --gas 300000 --node tcp://47.254.66.177:26657
 expect "Enter keyring passphrase (attempt 1/3):"
-send "$pwd\\n"
+send "$pwd\r"
 expect {
     -re {confirm transaction before signing and broadcasting \[y/N\]:\s*$} {
-        send "y\n"
+        send "y\r"
         exp_continue
     }
     eof
