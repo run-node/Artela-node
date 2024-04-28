ab168732ccbedeaf2f6a1c2313a00ad3849aa2a8
run-node <160773499+run-node@users.noreply.github.com>
2024-04-28 22:51:13 +0800

    Update art.sh

--- art.sh
+++ art.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-while true; do
-    try
-        expect -c '
-        spawn artelad tx staking delegate $validator $amountart --from $address --chain-id artela_11822-1 --gas 300000 --node tcp://47.254.66.177:26657
-        expect "Enter keyring passphrase (attempt 1/3):"
-        send "$pwd\r"
-        expect {
-            -re {confirm transaction before signing and broadcasting \[y/N\]:\s*$} {
-                send "y\r"
-                exp_continue
-            }
-            eof
-        }
-        '
-    catch
-        echo "Error occurred, but continuing..."
-    end
+# 关闭错误退出
+set +e
 
+while true; do
+    expect -c '
+    spawn artelad tx staking delegate $validator $amountart --from $address --chain-id artela_11822-1 --gas 300000 --node tcp://47.254.66.177:26657
+    expect "Enter keyring passphrase (attempt 1/3):"
+    send "$pwd\r"
+    expect {
+        -re {confirm transaction before signing and broadcasting \[y/N\]:\s*$} {
+            send "y\r"
+            exp_continue
+        }
+        eof
+    }
+    '
+    echo "将在24~28小时内继续自动质押art"
+    
     # 等待24到28小时
     sleep $((24 * 3600 + RANDOM % (4 * 3600)))
 done
