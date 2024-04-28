a971a8463ee1237b403d6408fddcf67398fd3d2a
run-node <160773499+run-node@users.noreply.github.com>
2024-04-28 22:42:17 +0800

    Update art.sh

--- art.sh
+++ art.sh
@@ -1,13 +1,17 @@
-# 使用expect命令输入密码
-expect -c '
-spawn artelad tx staking delegate $validator $amountart --from $address --chain-id artela_11822-1 --gas 300000 --node tcp://47.254.66.177:26657
-expect "Enter keyring passphrase (attempt 1/3):"
-send "$pwd\r"
-expect {
-    -re {confirm transaction before signing and broadcasting \[y/N\]:\s*$} {
-        send "y\r"
-        exp_continue
+#!/bin/bash
+
+while true; do
+    expect -c '
+    spawn artelad tx staking delegate $validator $amountart --from $address --chain-id artela_11822-1 --gas 300000 --node tcp://47.254.66.177:26657
+    expect "Enter keyring passphrase (attempt 1/3):"
+    send "$pwd\r"
+    expect {
+        -re {confirm transaction before signing and broadcasting \[y/N\]:\s*$} {
+            send "y\r"
+            exp_continue
+        }
+        eof
     }
-    eof
-}
-'
+    '
+    sleep $((24 * 3600 + RANDOM % (4 * 3600)))
+done
