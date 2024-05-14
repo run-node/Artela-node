0f16e097bbcb58efc9dc7b6ff9070eead5d059ef
run-node <160773499+run-node@users.noreply.github.com>
2024-05-14 15:58:09 +0800

    Update art.sh

--- art.sh
+++ art.sh
@@ -5,7 +5,7 @@ set +e
 
 while true; do
     expect -c '
-    spawn artelad tx staking delegate $validator $amountart --from $address --chain-id artela_11822-1 --gas 300000 --node tcp://47.254.66.177:26657
+    spawn artelad tx staking delegate $validator $amountart --from $address --chain-id artela_11822-1 --gas 300000 --node $Artela_RPC_PORT
     expect "Enter keyring passphrase (attempt 1/3):"
     send "$pwd\r"
     expect {
@@ -16,10 +16,10 @@ while true; do
         eof
     }
     '
-    echo "将在1~5小时内继续自动质押art"
+    echo "将在3~7小时内继续自动质押art"
     current_time=$(TZ=UTC-8 date +"%Y-%m-%d %H:%M:%S")
     echo "当前时间（UTC+8）: $current_time"
     
-    # 等待1到5小时
+    # 等待3到7小时
     sleep $((10800 + RANDOM % 25200))
 done
